1c93b49cf19762983a3b631695f397c70fb4d6cb
Drishti <82228411+Drishti9421@users.noreply.github.com>
2023-02-17 13:46:18 +0530

    Gradient, Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
 
 function App() {
-  return <div className="App">
-    <Login/>
+  return <div className="App font-ourfont">
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   </div>;
 }
 
